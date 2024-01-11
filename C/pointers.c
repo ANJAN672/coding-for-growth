c889420df9e18d53d5091a33875e88fdaf2f0437
ANJAN672 <rockyanjan672@gmail.com>
2024-01-11 21:49:41 +0530

    Pointers in C

--- C/pointers.c
+++ C/pointers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+//adding void function using pointers
 void update(int *a,int *b) {
     printf("%d\n",*a+*b);
     printf("%d\n",*a-*b);   
 }
-
+//pointers is represented or declared with a asterisk "*"
 int main() {
     int a, b;
     int *pa = &a, *pb = &b;
