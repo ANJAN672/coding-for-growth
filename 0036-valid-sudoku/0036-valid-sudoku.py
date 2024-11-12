06ee76a1bd6a96fe3fc395ea852a13e58facdde6
ANJAN <rockyanjan672@gmail.com>
2024-11-12 20:30:31 +0530

    Time: 7 ms (63.55%), Space: 11.5 MB (82.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,36 +1,38 @@
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        #validate rows
         for i in range(9):
-            s = set()
+            s=set()
             for j in range(9):
-                item = board[i][j]
+                item=board[i][j]
                 if item in s:
                     return False
-                elif item != '.':
+                elif item !='.':
                     s.add(item)
-        
-        # Validate Cols
+        #validate cols
         for i in range(9):
-            s = set()
+            s=set()
             for j in range(9):
-                item = board[j][i]
+                item=board[j][i]
                 if item in s:
                     return False
-                elif item != '.':
+                elif item !='.':
                     s.add(item)
-            
-        # Validate Boxes
-        starts = [(0, 0), (0, 3), (0, 6),
-                  (3, 0), (3, 3), (3, 6),
-                  (6, 0), (6, 3), (6, 6)]
-        
-        for i, j in starts:
-            s = set()
-            for row in range(i, i+3):
-                for col in range(j, j+3):
-                    item = board[row][col]
+        #3x3 boxes
+        starts=[(0,0),(0,3),(0,6),
+                (3,0),(3,3),(3,6),
+                (6,0),(6,3),(6,6)]
+        for i,j in starts:
+            s=set()
+            for row in range(i,i+3):
+                for col in range(j,j+3):
+                    item=board[row][col]
                     if item in s:
                         return False
-                    elif item != '.':
+                    elif item!='.':
                         s.add(item)
         return True
