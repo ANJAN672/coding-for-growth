77abd2793532d75d737f7ffa956caee7f08575f4
ANJAN <rockyanjan672@gmail.com>
2025-02-13 15:06:47 +0530

    Time: 412 ms (42.59%), Space: 33.7 MB (33.64%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,15 +1,10 @@
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        hashm = {} 
-        maxm = -1   
-        
-        for num in nums:
-            sumi = sum(map(int, str(num)))  
-            
-            if sumi in hashm:
-                maxm = max(maxm, num + hashm[sumi][0])
-                hashm[sumi] = [max(num, hashm[sumi][0]), min(num, hashm[sumi][0])]
-            else:
-                hashm[sumi] = [num] 
-                
-        return maxm
+        max_arr=[0]*82
+        ans=-1
+        for x in nums:
+            sumi=sum(map(int,str(x)))
+            if max_arr[sumi] !=0:
+                ans=max(ans,x+max_arr[sumi])
+            max_arr[sumi]=max(max_arr[sumi],x)
+        return ans
