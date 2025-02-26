6bad649100ad218c0f21df51f0cd0f124bd13cc8
ANJAN <rockyanjan672@gmail.com>
2025-02-26 17:47:10 +0530

    Time: 1 ms (80.1%), Space: 17.7 MB (82.04%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,17 +1,24 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
-        def dfs(prefix, n, k):
-            if n == 0:
-                return prefix
-            for c in 'abc':
-                if prefix and c == prefix[-1]:
-                    continue
-                cnt = 2 ** (n2 - len(prefix) - 1)
-                if cnt >= k:
-                    return dfs(prefix + c, n - 1, k)
-                else:
-                    k -= cnt
+        total_happy=3*(2**(n-1))
+        res=[]
+        left,right=1,total_happy
+        choices="abc"
+
+        if k>total_happy:
             return ""
-        
-        n2 = n
-        return dfs("", n, k)
+
+        #Partition
+        for i in range(n):
+            cur=left
+            partition_size=(right-left+1)//len(choices)
+            #To append to res
+            for c in choices:
+                if k in range(cur,cur+partition_size):
+                    res.append(c)
+                    left=cur
+                    right=cur+partition_size-1
+                    choices="abc".replace(c,"")
+                    break
+                cur+=partition_size
+        return "".join(res)
