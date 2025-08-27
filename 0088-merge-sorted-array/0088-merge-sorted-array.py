ee63b380e3ccb317a28eb24d4241ea312d62379a
Anjan B <rockyanjan672@gmail.com>
2025-08-27 19:35:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (12.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,22 +7,20 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        p1=m-1
-        p2=n-1
-        p=m+n-1
+        p1=m-1 #2
+        p2=n-1 #2
+        P=m+n-1 #5
 
         while p1>=0 and p2>=0:
             if nums2[p2]>nums1[p1]:
-                nums1[p]=nums2[p2]
+                nums1[P]=nums2[p2]
                 p2-=1
             else:
-                nums1[p]=nums1[p1]
+                nums1[P]=nums1[p1]
                 p1-=1
-            p-=1
+            P-=1
         while p2>=0:
-            nums1[p]=nums2[p2]
+            nums1[P]=nums2[p2]
             p2-=1
-            p-=1
+            P-=1
         return nums1
-
-                            
